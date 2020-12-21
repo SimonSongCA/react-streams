3dfc4974d374a0b1db5f80a3616f7abb70405110
SimonSongCA <SimonSongCA@gmail.com>
2020-12-21 14:46:08 -0800

    Creating a modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// create a modal using semantic-ui
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        laksjdflakjsdf lkajsldfkjalsfdj
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
