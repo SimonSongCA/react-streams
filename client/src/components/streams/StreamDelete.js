3534f0c845a27a0f3dba2fdd6a89b895dafc2563
SimonSongCA <SimonSongCA@gmail.com>
2020-12-22 14:16:27 -0800

    375 conditionally show the information when the user clicked the 'delete' button

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,31 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
 // 368 creating a modal
 import Modal from "../Modal";
 // 372 reconfigure the Modal components
 import history from "../../history";
+import { fetchStream } from "../../actions";
 
-const StreamDelete = () => {
-  const actions = (
-    // use React.Fragment to return multiple elements without its presence inside of DOM
-    // we could also use the empty tag '<> </>' but this could cause some tooling errors
-    // or might been seen as invalid by some code quality checkers
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
-  // return <div>StreamDelete</div>
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    return (
+      // use React.Fragment to return multiple elements without its presence inside of DOM
+      // we could also use the empty tag '<> </>' but this could cause some tooling errors
+      // or might been seen as invalid by some code quality checkers
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  // 375 conditionally display the information of the stream to be deleted
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream:";
+    }
+
+    return `Are you sure you want to delete the stream: ${this.props.stream.title} ?`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+// 375 Conditionally display the stream details
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
