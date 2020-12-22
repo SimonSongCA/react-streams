ddd4726d5557c19fc0baad20122f591c82886c68
SimonSongCA <SimonSongCA@gmail.com>
2020-12-22 14:40:07 -0800

    376 enabled the user to delete a stream or cancel the delete operation when prompted

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -34,29 +34,24 @@ export const signOut = () => {
 export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
-
   dispatch({ type: CREATE_STREAM, payload: response.data });
-
   // 352 navigate the user to the main page
   history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
   const response = await streams.get("/streams");
-
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 export const fetchStream = (id) => async (dispatch) => {
   const response = await streams.get(`/streams/${id}`);
-
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
   // 364 change .put to .patch
   const response = await streams.patch(`/streams/${id}`, formValues);
-
   dispatch({ type: EDIT_STREAM, payload: response.data });
   // 364
   history.push("/");
@@ -64,6 +59,7 @@ export const editStream = (id, formValues) => async (dispatch) => {
 
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
-
   dispatch({ type: DELETE_STREAM, payload: id });
+  // 375 delete the stream
+  history.push("/");
 };
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -5,20 +5,34 @@ import Modal from "../Modal";
 // 372 reconfigure the Modal components
 import history from "../../history";
 import { fetchStream } from "../../actions";
+// 376 delete the stream
+import { Link } from "react-router-dom";
+import { deleteStream } from "../../actions";
 
 class StreamDelete extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  // 375 use the action creator to delete the stream
   renderActions() {
+    // 375 introduce the id of the stream to be deleted
+    const { id } = this.props.match.params;
+
     return (
       // use React.Fragment to return multiple elements without its presence inside of DOM
       // we could also use the empty tag '<> </>' but this could cause some tooling errors
       // or might been seen as invalid by some code quality checkers
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   }
@@ -49,4 +63,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
