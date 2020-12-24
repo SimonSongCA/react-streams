b85daeec4416a14f7d6ae8b0fee15826a09a38a4
SimonSongCA <SimonSongCA@gmail.com>
2020-12-24 14:32:36 -0800

    380 enabled the user to enter a specific stream page when clicking on the title; used <Switch> tag to fix a bug where the user may see the {StreamShow} component when entering localhost:3000/streams/:id

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -17,11 +17,13 @@ const App = () => {
       <Router history={history}>
         <Header className="ui container" />
         <div>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
